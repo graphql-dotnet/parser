b30efd4c0e06d51b76b73cddf5a16870cfc3243e
Ivan Maximov <sungam3r@yandex.ru>
2019-12-22 01:41:22 +0300

    move TokenKind to separate file

--- src/GraphQLParser/Token.cs
+++ src/GraphQLParser/Token.cs
@@ -1,29 +1,5 @@
 ﻿namespace GraphQLParser
 {
-    public enum TokenKind
-    {
-        EOF = 1,
-        BANG = 2,
-        DOLLAR = 3,
-        PAREN_L = 4,
-        PAREN_R = 5,
-        SPREAD = 6,
-        COLON = 7,
-        EQUALS = 8,
-        AT = 9,
-        BRACKET_L = 10,
-        BRACKET_R = 11,
-        BRACE_L = 12,
-        PIPE = 13,
-        BRACE_R = 14,
-        NAME = 15,
-        INT = 16,
-        FLOAT = 17,
-        STRING = 18,
-        COMMENT = 19,
-        UNKNOWN = 20
-    }
-
     public readonly struct Token
     {
         public Token(TokenKind kind, string value, int start, int end)
---
+++ src/GraphQLParser/TokenKind.cs
@@ -0,0 +1,26 @@
+﻿namespace GraphQLParser
+{
+    public enum TokenKind
+    {
+        EOF = 1,
+        BANG = 2,
+        DOLLAR = 3,
+        PAREN_L = 4,
+        PAREN_R = 5,
+        SPREAD = 6,
+        COLON = 7,
+        EQUALS = 8,
+        AT = 9,
+        BRACKET_L = 10,
+        BRACKET_R = 11,
+        BRACE_L = 12,
+        PIPE = 13,
+        BRACE_R = 14,
+        NAME = 15,
+        INT = 16,
+        FLOAT = 17,
+        STRING = 18,
+        COMMENT = 19,
+        UNKNOWN = 20
+    }
+}
