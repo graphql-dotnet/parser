e81232236c50ac50d78868f1a87cf97f8b70e5bf
Ivan Maximov <sungam3r@yandex.ru>
2020-01-19 23:50:02 +0300

    move to new project

---
+++ src/GraphQLParser.ApiTests/ApiApprovalTests.cs
@@ -0,0 +1,18 @@
+﻿using PublicApiGenerator;
+using Shouldly;
+using Xunit;
+
+namespace GraphQLParser.ApiTests
+{
+    public class ApiApprovalTests
+    {
+        [Fact]
+        public void Public_Api_Should_Not_Change_Inadvertently()
+        {
+            typeof(Lexer).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                IncludeAssemblyAttributes = false
+            }).ShouldMatchApproved();
+        }
+    }
+}
