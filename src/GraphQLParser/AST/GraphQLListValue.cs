3ee929b7cd9c22c85ba7c2a0fc0baca43b351984
Ivan Maximov <sungam3r@yandex.ru>
2020-03-21 10:28:45 +0300

    naming

--- src/GraphQLParser/AST/GraphQLListValue.cs
+++ src/GraphQLParser/AST/GraphQLListValue.cs
@@ -4,16 +4,16 @@ namespace GraphQLParser.AST
 {
     public class GraphQLListValue : GraphQLValue
     {
-        private readonly ASTNodeKind _kindField;
+        private readonly ASTNodeKind _kind;
 
         public GraphQLListValue(ASTNodeKind kind)
         {
-            _kindField = kind;
+            _kind = kind;
         }
 
         public string? AstValue { get; set; }
 
-        public override ASTNodeKind Kind => _kindField;
+        public override ASTNodeKind Kind => _kind;
 
         public List<GraphQLValue>? Values { get; set; }
 
