c26e3e4e2c30544ba11f32a300c8d18057ab796d
Shane Krueger <shane@acdmail.com>
2020-08-15 13:30:17 -0400

    Update LexerValidationTests.cs

--- src/GraphQLParser.Tests/Validation/LexerValidationTests.cs
+++ src/GraphQLParser.Tests/Validation/LexerValidationTests.cs
@@ -71,7 +71,7 @@ namespace GraphQLParser.Tests.Validation
 1: \u0007
    ^
 ").Replace(Environment.NewLine, "\n"), exception.Message);
-            exception.Description.ShouldBe("TEST FAILURE");
+            exception.Description.ShouldBe("TEST FAILURE2");
             exception.Line.ShouldBe(1);
             exception.Column.ShouldBe(1);
         }
