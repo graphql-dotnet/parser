d896ac71fa8ab72dcb2d1f2812dff457a05b0472
Ivan Maximov <sungam3r@yandex.ru>
2020-08-16 15:11:42 +0300

    comments

--- src/GraphQLParser/AST/GraphQLLocation.cs
+++ src/GraphQLParser/AST/GraphQLLocation.cs
@@ -12,9 +12,25 @@ namespace GraphQLParser.AST
             End = end;
         }
 
+        /// <summary>
+        /// The index for the start of the node in the source (i.e. it's inclusive).
+        ///
+        /// For example,
+        ///     { field { subfield } }
+        ///       ^ field.Location.Start = 2
+        /// </summary>
+        public int Start { get; }
+
+        /// <summary>
+        /// The index for the character immediately after the node in the source (i.e. it's exclusive).
+        ///
+        /// For example,
+        ///     { field { subfield } }
+        ///                         ^ field.Location.End = 20
+        /// </summary>
         public int End { get; }
 
-        public int Start { get; }
+
 
         public bool Equals(GraphQLLocation other) => Start == other.Start && End == other.End;
 
