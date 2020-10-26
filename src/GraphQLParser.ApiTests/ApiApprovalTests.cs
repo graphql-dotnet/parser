496b1c95b3aa4e9274c19041f86d7b1a8feea68b
Ivan Maximov <sungam3r@yandex.ru>
2020-10-26 18:40:16 +0300

    1

--- src/GraphQLParser.ApiTests/ApiApprovalTests.cs
+++ src/GraphQLParser.ApiTests/ApiApprovalTests.cs
@@ -24,7 +24,7 @@ namespace GraphQLParser.ApiTests
             api.ShouldBe(approved);
             Console.WriteLine("OOOOOOOKKKKKKKKKKKK");
 
-            //api.ShouldMatchApproved(opt => opt.);
+            api.ShouldMatchApproved();
         }
     }
 }
