09359795ec6db52fcbabca8a678401755a30de72
Ivan Maximov <sungam3r@yandex.ru>
2020-10-26 18:58:28 +0300

    1

--- src/GraphQLParser.ApiTests/ApiApprovalTests.cs
+++ src/GraphQLParser.ApiTests/ApiApprovalTests.cs
@@ -12,19 +12,10 @@ namespace GraphQLParser.ApiTests
         [Fact]
         public void Public_Api_Should_Not_Change_Inadvertently()
         {
-            var api = typeof(Lexer).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            typeof(Lexer).Assembly.GeneratePublicApi(new ApiGeneratorOptions
             {
                 IncludeAssemblyAttributes = false
-            });
-
-            string a = AppDomain.CurrentDomain.BaseDirectory!;
-            Console.WriteLine("AAAA " + a);
-            var approved = File.ReadAllText(Path.Combine(a, "../../../ApiApprovalTests.Public_Api_Should_Not_Change_Inadvertently.approved.txt"));
-
-            api.ShouldBe(approved);
-            Console.WriteLine("OOOOOOOKKKKKKKKKKKK");
-
-            api.ShouldMatchApproved();
+            }).ShouldMatchApproved();
         }
     }
 }
