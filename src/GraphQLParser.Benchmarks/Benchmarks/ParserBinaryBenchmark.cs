f1c0d466970e8655d04fa6b66a761d0a5ac7ccb5
Ivan Maximov <sungam3r@yandex.ru>
2021-01-09 15:33:41 +0300

    Benchmarks rework (#99)

--- src/GraphQLParser.Benchmarks/Benchmarks/ParserBinaryBenchmark.cs
+++ src/GraphQLParser.Benchmarks/Benchmarks/ParserBinaryBenchmark.cs
@@ -1,23 +1,32 @@
-using System.IO;
 using BenchmarkDotNet.Attributes;
 using GraphQLParser.Exceptions;
 
 namespace GraphQLParser.Benchmarks
 {
     [MemoryDiagnoser]
-    public class ParserBinaryBenchmark
+    public class ParserBinaryBenchmark : IBenchmark
     {
+        private string _binaryTest = null!;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _binaryTest = "BinaryTest".ReadGraphQLFile();
+        }
+
         [Benchmark]
         public void ParseBinaryFile()
         {
             try
             {
                 var parser = new Parser(new Lexer());
-                parser.Parse(new Source(File.ReadAllText("BinaryTest.graphql")));
+                parser.Parse(new Source(_binaryTest));
             }
             catch (GraphQLSyntaxErrorException)
             {
             }
         }
+
+        void IBenchmark.Run() => ParseBinaryFile();
     }
 }
