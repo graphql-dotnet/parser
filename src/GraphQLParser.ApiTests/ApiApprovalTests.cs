b7e521ea60083eaeb92807e5a4a14381258a5cb9
Ivan Maximov <sungam3r@yandex.ru>
2021-01-17 00:16:39 +0300

    add dotnet-format

--- src/GraphQLParser.ApiTests/ApiApprovalTests.cs
+++ src/GraphQLParser.ApiTests/ApiApprovalTests.cs
@@ -1,3 +1,4 @@
+using System;
 using PublicApiGenerator;
 using Shouldly;
 using Xunit;
@@ -6,13 +7,22 @@ namespace GraphQLParser.ApiTests
 {
     public class ApiApprovalTests
     {
-        [Fact]
-        public void Public_Api_Should_Not_Change_Inadvertently()
+        [Theory]
+        [InlineData(typeof(Lexer))]
+        public void Public_Api_Should_Not_Change_Inadvertently(Type type)
         {
-            typeof(Lexer).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            string publicApi = type.Assembly.GeneratePublicApi(new ApiGeneratorOptions
             {
-                IncludeAssemblyAttributes = false
-            }).ShouldMatchApproved();
+                IncludeAssemblyAttributes = false,
+                //WhitelistedNamespacePrefixes = new[] { "Microsoft.Extensions.DependencyInjection" },
+                ExcludeAttributes = new[] { "System.Diagnostics.DebuggerDisplayAttribute" }
+            });
+
+            // See: https://shouldly.readthedocs.io/en/latest/assertions/shouldMatchApproved.html
+            // Note: If the AssemblyName.approved.txt file doesn't match the latest publicApi value,
+            // this call will try to launch a diff tool to help you out but that can fail on
+            // your machine if a diff tool isn't configured/setup. 
+            publicApi.ShouldMatchApproved(options => options.WithFilenameGenerator((testMethodInfo, discriminator, fileType, fileExtension) => $"{type.Assembly.GetName().Name}.{fileType}.{fileExtension}"));
         }
     }
 }
