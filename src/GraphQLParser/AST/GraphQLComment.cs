60a9902696bb8cb3ce364d583b9ac9e847646f72
Ivan Maximov <sungam3r@yandex.ru>
2021-01-21 17:47:45 +0300

    Remove string values

--- src/GraphQLParser/AST/GraphQLComment.cs
+++ src/GraphQLParser/AST/GraphQLComment.cs
@@ -3,33 +3,15 @@ using System.Diagnostics;
 namespace GraphQLParser.AST
 {
     /// <inheritdoc cref="ASTNodeKind.Comment"/>
-    [DebuggerDisplay("{TextString}")]
+    [DebuggerDisplay("{Text}")]
     public class GraphQLComment : ASTNode
     {
-        private ROM _text;
-        private string? _textString;
-
         /// <inheritdoc/>
         public override ASTNodeKind Kind => ASTNodeKind.Comment;
 
         /// <summary>
         /// Comment value represented as <see cref="ROM"/>.
         /// </summary>
-        public ROM Text
-        {
-            get => _text;
-            set
-            {
-                _text = value;
-                _textString = null;
-            }
-        }
-
-        /// <summary>
-        /// Gets comment value represented as string. The value of this property is cached and in sync with <see cref="Text"/>.
-        /// The first time this property is accessed, memory in the managed heap will be allocated for it.
-        /// In scenarios where minimum memory consumption is required, use the <see cref="Text"/> property.
-        /// </summary>
-        public string TextString => _textString ??= (string)Text;
+        public ROM Text { get; set; }
     }
 }
