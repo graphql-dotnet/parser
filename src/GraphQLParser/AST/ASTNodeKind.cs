5cc843edef9977d605ada3dfd6ceb65c4472c0ec
Ivan Maximov <sungam3r@yandex.ru>
2021-02-01 22:03:27 +0300

    Update src/GraphQLParser/AST/ASTNodeKind.cs

--- src/GraphQLParser/AST/ASTNodeKind.cs
+++ src/GraphQLParser/AST/ASTNodeKind.cs
@@ -74,7 +74,6 @@ namespace GraphQLParser.AST
         InputObjectTypeDefinition,
         TypeExtensionDefinition,
 
-
         /// <summary>
         /// Directive definition. Directives provide a way to describe alternate runtime execution
         /// and type validation behavior in a GraphQL document. Directives can be used to describe
