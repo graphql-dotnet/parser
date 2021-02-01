b12f995d0dc4cac64633b79079aacd27b47f49cf
Ivan Maximov <sungam3r@yandex.ru>
2021-02-01 17:38:12 +0300

    test

--- src/GraphQLParser.Tests/MemoryTests.cs
+++ src/GraphQLParser.Tests/MemoryTests.cs
@@ -10,12 +10,20 @@ namespace GraphQLParser.Tests
         {
             var str = "string";
             ROM rom = str;
+            rom.IsEmpty.ShouldBeFalse();
+            rom.Length.ShouldBe(6);
+            rom.GetHashCode().ShouldNotBe(0);
+
             (rom == str).ShouldBeTrue();
             (str == rom).ShouldBeTrue();
             (rom != str).ShouldBeFalse();
             (str != rom).ShouldBeFalse();
 
             var rom2 = rom.Slice(1);
+            rom2.IsEmpty.ShouldBeFalse();
+            rom2.Length.ShouldBe(5);
+            rom2.GetHashCode().ShouldNotBe(0);
+            rom2.GetHashCode().ShouldNotBe(rom.GetHashCode());
 
             (rom2 == str).ShouldBeFalse();
             (str == rom2).ShouldBeFalse();
