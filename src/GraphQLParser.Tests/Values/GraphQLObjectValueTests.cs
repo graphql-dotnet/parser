b6f7fc24515d5edf1aa87bf8aed032486ebde7a5
Ivan Maximov <sungam3r@yandex.ru>
2022-01-17 22:08:21 +0300

    test

--- src/GraphQLParser.Tests/Values/GraphQLObjectValueTests.cs
+++ src/GraphQLParser.Tests/Values/GraphQLObjectValueTests.cs
@@ -22,7 +22,7 @@ public class GraphQLObjectValueTests
     }
 
     [Fact]
-    public void Field_From_FilledFields()
+    public void Field_From_FilledFields_FirstFound()
     {
         var obj = new GraphQLObjectValue
         {
@@ -30,6 +30,23 @@ public class GraphQLObjectValueTests
             {
                 new GraphQLObjectField { Name = new GraphQLName("abc"), Value = new GraphQLIntValue(42) },
                 new GraphQLObjectField { Name = new GraphQLName("def"), Value = new GraphQLIntValue(420) },
+                new GraphQLObjectField { Name = new GraphQLName("abc"), Value = new GraphQLIntValue(4200) },
+            }
+        };
+        var field = obj.Field("abc");
+        field.Name.ShouldBe(new GraphQLName("abc"));
+        field.Value.ShouldBeOfType<GraphQLIntValue>().Value.ShouldBe("42");
+    }
+
+    [Fact]
+    public void Field_From_FilledFields_SecondFound()
+    {
+        var obj = new GraphQLObjectValue
+        {
+            Fields = new List<GraphQLObjectField>
+            {
+                new GraphQLObjectField { Name = new GraphQLName("def"), Value = new GraphQLIntValue(420) },
+                new GraphQLObjectField { Name = new GraphQLName("abc"), Value = new GraphQLIntValue(42) },
                 new GraphQLObjectField { Name = new GraphQLName("abc"), Value = new GraphQLIntValue(4200) },
             }
         };
