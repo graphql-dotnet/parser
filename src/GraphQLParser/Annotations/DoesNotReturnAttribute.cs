b80fe5b8473305d36ec3608b924e75363f57761b
Ivan Maximov <sungam3r@yandex.ru>
2022-01-22 15:27:14 +0300

    link

--- src/GraphQLParser/Annotations/DoesNotReturnAttribute.cs
+++ src/GraphQLParser/Annotations/DoesNotReturnAttribute.cs
@@ -1,6 +1,6 @@
 #if NETSTANDARD2_0
 
-// https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs#L69
+// https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs#L107
 namespace System.Diagnostics.CodeAnalysis;
 
 /// <summary>Applied to a method that will never return under any circumstance.</summary>
