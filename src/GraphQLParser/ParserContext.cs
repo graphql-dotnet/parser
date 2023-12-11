c14506708f969a45a43469c3c171424a6aa06bcd
Ivan Maximov <sungam3r@yandex.ru>
2023-12-11 12:23:21 +0300

    format

--- src/GraphQLParser/ParserContext.cs
+++ src/GraphQLParser/ParserContext.cs
@@ -46,28 +46,28 @@ internal ref partial struct ParserContext
 
     private static string[] DirectiveLocationOneOf { get; set; } =
     [
-            // http://spec.graphql.org/June2018/#ExecutableDirectiveLocation
-            "QUERY",
-            "MUTATION",
-            "SUBSCRIPTION",
-            "FIELD",
-            "FRAGMENT_DEFINITION",
-            "FRAGMENT_SPREAD",
-            "INLINE_FRAGMENT",
-            "VARIABLE_DEFINITION",
-            // http://spec.graphql.org/June2018/#TypeSystemDirectiveLocation
-            "SCHEMA",
-            "SCALAR",
-            "OBJECT",
-            "FIELD_DEFINITION",
-            "ARGUMENT_DEFINITION",
-            "INTERFACE",
-            "UNION",
-            "ENUM",
-            "ENUM_VALUE",
-            "INPUT_OBJECT",
-            "INPUT_FIELD_DEFINITION",
-        ];
+        // http://spec.graphql.org/June2018/#ExecutableDirectiveLocation
+        "QUERY",
+        "MUTATION",
+        "SUBSCRIPTION",
+        "FIELD",
+        "FRAGMENT_DEFINITION",
+        "FRAGMENT_SPREAD",
+        "INLINE_FRAGMENT",
+        "VARIABLE_DEFINITION",
+        // http://spec.graphql.org/June2018/#TypeSystemDirectiveLocation
+        "SCHEMA",
+        "SCALAR",
+        "OBJECT",
+        "FIELD_DEFINITION",
+        "ARGUMENT_DEFINITION",
+        "INTERFACE",
+        "UNION",
+        "ENUM",
+        "ENUM_VALUE",
+        "INPUT_OBJECT",
+        "INPUT_FIELD_DEFINITION",
+    ];
 
     private delegate TResult ParseCallback<out TResult>(ref ParserContext context);
 
